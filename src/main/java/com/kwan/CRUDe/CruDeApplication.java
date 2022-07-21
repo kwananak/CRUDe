f0c0a212589f958fd7e310eaf8f9d071f71623b9
kwananak <kwananak@protonmail.com>
2022-07-21 11:33:10 -0400

    First commit from Eclipse

---
+++ src/main/java/com/kwan/CRUDe/CruDeApplication.java
@@ -0,0 +1,13 @@
+package com.kwan.CRUDe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruDeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruDeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kwan/CRUDe/CruDeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kwan.CRUDe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruDeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
